b5ed39f77d280d23df9529764735b69aede968db
rab123 <rab123@uslex.net>
2011-04-15 20:25:17 -0500

    added video model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Qv::Application.routes.draw do
+  resources :videos
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
---
+++ spec/views/videos/show.html.erb_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe "videos/show.html.erb" do
+  before(:each) do
+    @video = assign(:video, stub_model(Video,
+      :title => "Title",
+      :description => "Description",
+      :category => "Category",
+      :subject => "Subject",
+      :url => "Url",
+      :embed_code => "MyText",
+      :age_level => 1,
+      :rating => 1,
+      :finder_id => 1
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    rendered.should match(/Title/)
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    rendered.should match(/Description/)
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    rendered.should match(/Category/)
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    rendered.should match(/Subject/)
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    rendered.should match(/Url/)
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    rendered.should match(/MyText/)
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    rendered.should match(/1/)
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    rendered.should match(/1/)
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    rendered.should match(/1/)
+  end
+end
